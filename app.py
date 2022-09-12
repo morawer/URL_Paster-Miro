2d8486f187992a333c29315e9b68f6dcb4af4ba8
morawer <morala84@gmail.com>
2022-09-12 19:58:15 +0200

    date filter created

--- app.py
+++ app.py
@@ -2,20 +2,42 @@ import os
 import json
 import requests
 from dotenv import load_dotenv
+from datetime import datetime, timedelta, time
+import pytz
 
 load_dotenv()
 
-count = 0
 
 token = os.getenv("TOKEN_NOTION")
 database_notion = os.getenv("DATABASE_NOTION")
 
+count = 0
+
+UTC = pytz.utc
+date = datetime.now(UTC) - timedelta(minutes=30)
+dateStr = str(date)
+date2 = dateStr.split('.')[0]
+dateFinal = date2.replace(' ', 'T')
+
+print(dateFinal)
+
 url = f"https://api.notion.com/v1/databases/{database_notion}/query"
 
-payload={}
+payload = json.dumps( {
+    "filter":   {
+        "property": "Creado",
+                    "created_time": {
+                        "after": dateFinal
+                    }
+    }
+})
+  
+
 headers = {
   'Notion-Version': '2022-06-28',
-  'Authorization': token
+  'Authorization': token,
+  'Content-Type': 'application/json'
+
 }
 
 response = requests.request("POST", url, headers=headers, data=payload)
@@ -26,4 +48,5 @@ print(response.status_code)
 
 for data in jsonResponse['results']:
   count = count + 1
-  print(f"{count} URL: {data['url']}")
+  print(
+      f"{count} URL: {data['url']} DATE:{data['properties']['Creado']['created_time']}")
