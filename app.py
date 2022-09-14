4283fdd27d7983988350263c8b8439d9cb7c2a15
Daniel Morala <morala84@gmail.com>
2022-09-14 16:12:46 +0200

    tests

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ load_dotenv()
 
 token = os.getenv("TOKEN_NOTION")
 database_notion = os.getenv("DATABASE_NOTION")
+tokenMiro = os.getenv("TOKEN_MIRO")
 
 count = 0
 
 UTC = pytz.utc
-date = datetime.now(UTC) - timedelta(minutes=30)
+date = datetime.now(UTC) - timedelta(minutes=1030)
 dateStr = str(date)
 date2 = dateStr.split('.')[0]
 dateFinal = date2.replace(' ', 'T')
@@ -48,5 +49,32 @@ print(response.status_code)
 
 for data in jsonResponse['results']:
   count = count + 1
+  titleNotion = data['properties']['Pedido']['title'][0]['plain_text']
   print(
-      f"{count} URL: {data['url']} DATE:{data['properties']['Creado']['created_time']}")
+      f"{count} Pedido: {titleNotion} URL: {data['url']} DATE:{data['properties']['Creado']['created_time']}")
+
+
+  url = "https://api.miro.com/v2/boards/o9J_kjQ2bCw=/items?limit=50&type=shape"
+
+  payload={}
+  headers = {
+  'Authorization': tokenMiro,
+  'Cookie': 'AWSALBTG=ywUl6gU/VxJCBAF9IIiWuTQ6PQ1HzIsZgkvCGT2uP6Ya6olyQUqCAEhbEMWpTA55jhs0SVYZHqFkoxd7FCdepx11lJJ9O19F1fcutTRiQ9XAOWO2lpn5n9vL/s/cF39ezgOKBmYOZ9nTzPS254Uyb0awY14VZBemCBm3Y1qoPQ9p; AWSALBTGCORS=ywUl6gU/VxJCBAF9IIiWuTQ6PQ1HzIsZgkvCGT2uP6Ya6olyQUqCAEhbEMWpTA55jhs0SVYZHqFkoxd7FCdepx11lJJ9O19F1fcutTRiQ9XAOWO2lpn5n9vL/s/cF39ezgOKBmYOZ9nTzPS254Uyb0awY14VZBemCBm3Y1qoPQ9p'
+}
+
+  response = requests.request("GET", url, headers=headers, data=payload)
+
+  jsonResponse = json.loads(response.text)
+
+
+  title = jsonResponse['data'][0]['data']['content']
+
+  print( title.__contains__(titleNotion))
+
+  
+
+  
+  
+
+
+
