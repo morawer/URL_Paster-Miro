d01f0e27fb33ac6cc59f66944a9c7add33ceae1f
Daniel Morala <morala84@gmail.com>
2022-09-15 16:12:07 +0200

    test

--- app.py
+++ app.py
@@ -14,17 +14,76 @@ tokenMiro = os.getenv("TOKEN_MIRO")
 
 count = 0
 
+counter = 0
 UTC = pytz.utc
-date = datetime.now(UTC) - timedelta(minutes=1030)
+date = datetime.now(UTC) - timedelta(minutes=60)
 dateStr = str(date)
 date2 = dateStr.split('.')[0]
 dateFinal = date2.replace(' ', 'T')
 
-print(dateFinal)
+def QueryMiro(tokenMiro, counter, titleNotion):
+    url = "https://api.miro.com/v2/boards/o9J_kjQ2bCw=/items?limit=50&type=sticky_note"
 
-url = f"https://api.notion.com/v1/databases/{database_notion}/query"
+    payload={}
+    headers = {
+  'Authorization': tokenMiro,
+  'Cookie': 'AWSALBTG=ywUl6gU/VxJCBAF9IIiWuTQ6PQ1HzIsZgkvCGT2uP6Ya6olyQUqCAEhbEMWpTA55jhs0SVYZHqFkoxd7FCdepx11lJJ9O19F1fcutTRiQ9XAOWO2lpn5n9vL/s/cF39ezgOKBmYOZ9nTzPS254Uyb0awY14VZBemCBm3Y1qoPQ9p; AWSALBTGCORS=ywUl6gU/VxJCBAF9IIiWuTQ6PQ1HzIsZgkvCGT2uP6Ya6olyQUqCAEhbEMWpTA55jhs0SVYZHqFkoxd7FCdepx11lJJ9O19F1fcutTRiQ9XAOWO2lpn5n9vL/s/cF39ezgOKBmYOZ9nTzPS254Uyb0awY14VZBemCBm3Y1qoPQ9p'
+  }
 
-payload = json.dumps( {
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    jsonResponseMiro = json.loads(response.text)
+
+
+    for itemMiro in jsonResponseMiro['data']:
+      title = itemMiro['data']['content']
+
+      if title.__contains__(titleNotion):
+        print( title.__contains__(titleNotion))
+        print(f'{title}  <<<<<  {titleNotion}')
+        break
+
+    nextLink = jsonResponseMiro['links']['next']
+    
+
+    QueryNextLink(tokenMiro, jsonResponseMiro, nextLink, titleNotion)
+
+def QueryNextLink(tokenMiro, jsonResponseMiro, nextLink, titleNotion):
+    url = nextLink
+
+    payload={}
+    headers = {
+      'Authorization': tokenMiro,
+      'Cookie': 'AWSALBTG=CaJQ225fs+q/WSOjddI8TCtHxmo+gloZhND5J7dfAV9zX8Dr66L3Zi+pfoZ2IN7bgyB08CLEMfeGxzqpNye4OL13mL1OUW99U4MRwX8u4oZWiYygaE8bfOBx17wiTmrS+z0aH7QGEorc+CkNYasIYWUim0id0OpKDiPwWWiG1L3o; AWSALBTGCORS=CaJQ225fs+q/WSOjddI8TCtHxmo+gloZhND5J7dfAV9zX8Dr66L3Zi+pfoZ2IN7bgyB08CLEMfeGxzqpNye4OL13mL1OUW99U4MRwX8u4oZWiYygaE8bfOBx17wiTmrS+z0aH7QGEorc+CkNYasIYWUim0id0OpKDiPwWWiG1L3o'
+      }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    jsonResponseMiro = json.loads(response.text)
+
+    for itemMiro in jsonResponseMiro['data']:
+      title = itemMiro['data']['content']        
+
+      if title.__contains__(titleNotion):
+        print( title.__contains__(titleNotion))
+        print(f'{title}  <<<<<  {titleNotion}')
+        break
+
+      
+
+
+
+    try:
+      nextLink = jsonResponseMiro['links']['next']
+  
+      QueryNextLink(tokenMiro, jsonResponseMiro, nextLink, titleNotion)
+    except:
+      print('FIN')
+
+def QueryNotion(token, database_notion, tokenMiro, count, counter, dateFinal, QueryMiro):
+    url = f"https://api.notion.com/v1/databases/{database_notion}/query"
+
+    payload = json.dumps( {
     "filter":   {
         "property": "Creado",
                     "created_time": {
@@ -33,48 +92,25 @@ payload = json.dumps( {
     }
 })
   
-
-headers = {
+    headers = {
   'Notion-Version': '2022-06-28',
   'Authorization': token,
   'Content-Type': 'application/json'
-
 }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.request("POST", url, headers=headers, data=payload)
 
-jsonResponse = json.loads(response.text)
+    jsonResponse = json.loads(response.text)
 
-print(response.status_code)
+    print(response.status_code)
 
-for data in jsonResponse['results']:
-  count = count + 1
-  titleNotion = data['properties']['Pedido']['title'][0]['plain_text']
-  print(
+    for data in jsonResponse['results']:
+      count = count + 1
+      titleNotion = data['properties']['Pedido']['title'][0]['plain_text']
+      print(
       f"{count} Pedido: {titleNotion} URL: {data['url']} DATE:{data['properties']['Creado']['created_time']}")
 
 
-  url = "https://api.miro.com/v2/boards/o9J_kjQ2bCw=/items?limit=50&type=shape"
+      QueryMiro(tokenMiro, counter, titleNotion)
 
-  payload={}
-  headers = {
-  'Authorization': tokenMiro,
-  'Cookie': 'AWSALBTG=ywUl6gU/VxJCBAF9IIiWuTQ6PQ1HzIsZgkvCGT2uP6Ya6olyQUqCAEhbEMWpTA55jhs0SVYZHqFkoxd7FCdepx11lJJ9O19F1fcutTRiQ9XAOWO2lpn5n9vL/s/cF39ezgOKBmYOZ9nTzPS254Uyb0awY14VZBemCBm3Y1qoPQ9p; AWSALBTGCORS=ywUl6gU/VxJCBAF9IIiWuTQ6PQ1HzIsZgkvCGT2uP6Ya6olyQUqCAEhbEMWpTA55jhs0SVYZHqFkoxd7FCdepx11lJJ9O19F1fcutTRiQ9XAOWO2lpn5n9vL/s/cF39ezgOKBmYOZ9nTzPS254Uyb0awY14VZBemCBm3Y1qoPQ9p'
-}
-
-  response = requests.request("GET", url, headers=headers, data=payload)
-
-  jsonResponse = json.loads(response.text)
-
-
-  title = jsonResponse['data'][0]['data']['content']
-
-  print( title.__contains__(titleNotion))
-
-  
-
-  
-  
-
-
-
+QueryNotion(token, database_notion, tokenMiro, count, counter, dateFinal, QueryMiro)
