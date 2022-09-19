a157ac536406c9942cbe56974a31eda4ba5c02f5
morawer <morala84@gmail.com>
2022-09-19 19:06:13 +0200

    signing_class.py deleted

--- signing_class.py
+++
@@ -1,9 +0,0 @@
-class Signing:
-    def __init__ (signing, id, createdTime, finishTime, workplace, worker, hours):
-
-        signing.id = id
-        signing.createdTime = createdTime
-        signing.finishTime = finishTime
-        signing.workplace = workplace
-        signing.worker = worker
-        signing.hours = hours
